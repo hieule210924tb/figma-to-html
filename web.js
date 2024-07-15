45aed4f763c04940f734ff88caa3e39cf82bcc69
hieu17 <leh364358@gmail.com>
2024-07-15 16:29:24 +0700

    add slide pôpular

--- web.js
+++ web.js
@@ -1,3 +1,31 @@
+// slide popular
+var slidesPo= document.querySelector(".course-list_slide");
+var prevButton = document.querySelector('.prev');
+console.log(prevButton);
+var nextButton = document.querySelector('.next');
+var slideCounts= document.querySelectorAll(".course-item").length;
+var slidesToShow = 3;
+var slideWidth = 100 / slidesToShow;
+let currentIndextSlide=0;
+
+function showSlidePo(index){
+    if(index >= slideCounts){
+       currentIndextSlide =0;
+    }else if(index<0){
+        currentIndextSlide = slideCounts - slidesToShow;
+    }else{
+        currentIndextSlide = index;
+    }
+    slidesPo.style.transform = `translateX(-${currentIndextSlide * slideWidth}%)`;
+}
+prevButton.addEventListener('click' ,() =>{
+    showSlidePo(currentIndextSlide -1);
+})
+nextButton.addEventListener('click', () => {
+    showSlidePo(currentIndextSlide + 1);
+});
+
+// slide feedback
 var slides  = document.querySelector(".show-sliders")
 var dots = document.querySelectorAll('.dot');
 var slideCount = document.querySelectorAll('.slide').length;
